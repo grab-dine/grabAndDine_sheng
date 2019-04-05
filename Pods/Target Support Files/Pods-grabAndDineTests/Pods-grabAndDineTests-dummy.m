8c49b5342039c6057f289b65b606ce12d29d8aba
Sheng Liu <sheng1302@gmail.com>
2019-04-05 18:52:52 -0400

    pod file

---
+++ Pods/Target Support Files/Pods-grabAndDineTests/Pods-grabAndDineTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_grabAndDineTests : NSObject
+@end
+@implementation PodsDummy_Pods_grabAndDineTests
+@end
---
+++ Pods/Target Support Files/Pods-grabAndDineUITests/Pods-grabAndDineUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_grabAndDineUITests : NSObject
+@end
+@implementation PodsDummy_Pods_grabAndDineUITests
+@end
